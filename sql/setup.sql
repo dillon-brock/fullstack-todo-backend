3614cc6fcdfc0bd6d556d63e060e00412018ec28
Dillon Brock <mdillon1297@gmail.com>
2022-08-22 15:11:06 -0700

    wrote sql to generate users and tasks tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+CREATE TABLE items (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  task TEXT NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT(false),
+  user_id BIGINT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
