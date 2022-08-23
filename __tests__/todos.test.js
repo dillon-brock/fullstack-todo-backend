0804b9535175fe3a72cdadc89787b88d293f3b6d
Dillon Brock <mdillon1297@gmail.com>
2022-08-23 14:23:58 -0700

    put route for updating todo passing tests

--- __tests__/todos.test.js
+++ __tests__/todos.test.js
@@ -16,13 +16,11 @@ const registerAndLogin = async (userProps = {}) => {
 
   const agent = request.agent(app);
 
-  const user = await UserService.create({ ...userProps, ...testUser });
+  const user = await UserService.create({ ...testUser, ...userProps });
 
-  const { firstName, lastName, email } = user;
-  await agent
-    .post('/api/v1/users')
-    .send({ firstName, lastName, email, password });
-  return [agent, user];
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return agent;
 };
 
 describe('todos', () => {
@@ -33,7 +31,7 @@ describe('todos', () => {
     pool.end();
   });
   it('#POST /api/v1/todos should create a new todo for the current user', async () => {
-    const [agent] = await registerAndLogin();
+    const agent = await registerAndLogin();
     const todo = { task: 'debug issue with cookies' };
     const res = await agent.post('/api/v1/todos').send(todo);
     expect(res.status).toBe(200);
@@ -44,13 +42,18 @@ describe('todos', () => {
       completed: false,
     });
   });
+  it('#POST /api/v1/todos should give 401 if user is not signed in', async () => {
+    const todo = { task: 'debug issue with cookies' };
+    const res = await request(app).post('/api/v1/todos').send(todo);
+    expect(res.status).toBe(401);
+  });
   it('#GET /api/v1/todos should return a list of todos for current user', async () => {
     const todos = [
       { task: 'laundry' },
       { task: 'restring guitar' },
       { task: 'make the bed' },
     ];
-    const [agent] = await registerAndLogin();
+    const agent = await registerAndLogin();
     for (const todo of todos) {
       await agent.post('/api/v1/todos').send(todo);
     }
@@ -62,5 +65,53 @@ describe('todos', () => {
       completed: false,
       user_id: expect.any(String),
     });
+  });
+  it('#GET /api/v1/todos should give 401 if user is not signed in', async () => {
+    const res = await request(app).get('/api/v1/todos');
+    expect(res.status).toBe(401);
+  });
+  it('#PUT /api/v1/todos/:id should update a todo with id matching params', async () => {
+    const mockUser = {
+      firstName: 'Mock',
+      lastName: 'User',
+      email: 'mockuser@test.com',
+      password: 'abc123',
+    };
+    const todo = { task: 'take a nap' };
+    const agent = await registerAndLogin(mockUser);
+    const postedTodo = await agent.post('/api/v1/todos').send(todo);
+    const res = await agent
+      .put(`/api/v1/todos/${postedTodo.body.id}`)
+      .send({ completed: true });
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      task: 'take a nap',
+      id: expect.any(String),
+      completed: true,
+      user_id: expect.any(String),
+    });
+  });
+  it('#PUT should give a 403 if invalid user', async () => {
+    const todo = { task: 'water plants' };
+    const mockUser = {
+      firstName: 'Mock',
+      lastName: 'User',
+      email: 'mockuser@test.com',
+      password: 'abc123',
+    };
+    const postAgent = await registerAndLogin();
+    const newTodo = await postAgent.post('/api/v1/todos').send(todo);
+    const updateAgent = await registerAndLogin(mockUser);
+    const res = await updateAgent
+      .put(`/api/v1/todos/${newTodo.body.id}`)
+      .send({ completed: true });
+    expect(res.status).toBe(403);
+  });
+  it('#PUT should give 401 if user is not signed in', async () => {
+    const agent = await registerAndLogin();
+    const todo = { task: 'take out trash' };
+    const newTodo = await agent.post('/api/v1/todos').send(todo);
+    const res = await request(app).put(`/api/v1/todos/${newTodo.body.id}`);
+    expect(res.status).toBe(401);
   });
 });
