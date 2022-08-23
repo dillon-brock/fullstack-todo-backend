a6a9e645ec5824d8552604b9c2d055040cde2e69
Dillon Brock <mdillon1297@gmail.com>
2022-08-22 17:06:48 -0700

    route to return current user passing tests

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,12 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const testUser = {
   firstName: 'Test',
   lastName: 'User',
   email: 'testuser@example.com',
   password: 'abc123',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...userProps, ...testUser });
+
+  const { firstName, lastName, email } = user;
+  await agent
+    .post('/api/v1/users')
+    .send({ firstName, lastName, email, password });
+  return agent;
 };
 
 describe('user routes', () => {
@@ -22,6 +37,23 @@ describe('user routes', () => {
       firstName: 'Test',
       lastName: 'User',
     });
+  });
+  it('#GET /api/v1/users/me should return current user', async () => {
+    const agent = await registerAndLogin();
+    const res = await agent.get('/api/v1/users/me');
+    const { firstName, lastName, email } = testUser;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+  it('#GET /api/v1/users/me should give 401 if no user is signed in', async () => {
+    const res = await request(app).get('/api/v1/users/me');
+    expect(res.status).toBe(401);
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,7 +11,7 @@ CREATE TABLE users (
   password_hash TEXT NOT NULL
 );
 
-CREATE TABLE items (
+CREATE TABLE tasks (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   task TEXT NOT NULL,
   completed BOOLEAN NOT NULL DEFAULT(false),
